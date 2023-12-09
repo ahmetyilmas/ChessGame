a661d9d8524050a2942cb03d42e0cf1d23b3550e
ahmetyilmas <ahmetyilmas03@gmail.com>
2023-12-09 19:59:05 +0300

    Taslar olusturuldu

--- ChessGame/src/Board.java
+++ ChessGame/src/Board.java
@@ -3,22 +3,61 @@
  * */
 public class Board {
     private Cell[][] cells;
-    private Cell cell;
 
     public Board(){
-
-    }
-    public void addCellToBoard(Cell cell, int positionX, int positionY){
-        this.cells[positionX][positionY] = cell;
+        this.resetBoard();
     }
     public Cell getCell(int x, int y){
-        Cell cellToReturn = this.cells[x][y];
-        return cellToReturn;
+        return this.cells[x][y];
 
     }
 
     public void resetBoard(){
-        cells[1][0] = new Cell(1, 0, new Pawn(true));
-        addCellToBoard(new Cell(1, 1, new Pawn(true)), 1, 1);
+
+        //  Siyah taslarin baslatilmasi
+        cells[0][0] = new Cell(0, 0, new Rook(false));
+        cells[0][1] = new Cell(0, 1, new Knight(false));
+        cells[0][2] = new Cell(0, 2, new Bishop(false));
+        cells[0][3] = new Cell(0, 3, new King(false));
+        cells[0][4] = new Cell(0, 4, new Queen(false));
+        cells[0][5] = new Cell(0, 5, new Bishop(false));
+        cells[0][6] = new Cell(0, 6, new Knight(false));
+        cells[0][7] = new Cell(0, 7, new Rook(false));
+
+        cells[1][0] = new Cell(0, 0, new Pawn(false));
+        cells[1][1] = new Cell(0, 0, new Pawn(false));
+        cells[1][2] = new Cell(0, 0, new Pawn(false));
+        cells[1][3] = new Cell(0, 0, new Pawn(false));
+        cells[1][4] = new Cell(0, 0, new Pawn(false));
+        cells[1][5] = new Cell(0, 0, new Pawn(false));
+        cells[1][6] = new Cell(0, 0, new Pawn(false));
+        cells[1][7] = new Cell(0, 0, new Pawn(false));
+
+
+        //  Beyaz taslarin baslatilmasi.
+        cells[6][0] = new Cell(6, 0, new Pawn(true));
+        cells[6][1] = new Cell(6, 1, new Pawn(true));
+        cells[6][2] = new Cell(6, 2, new Pawn(true));
+        cells[6][3] = new Cell(6, 3, new Pawn(true));
+        cells[6][4] = new Cell(6, 4, new Pawn(true));
+        cells[6][5] = new Cell(6, 5, new Pawn(true));
+        cells[6][6] = new Cell(6, 6, new Pawn(true));
+        cells[6][7] = new Cell(6, 7, new Pawn(true));
+
+        cells[7][0] = new Cell(7, 0, new Rook(true));
+        cells[7][1] = new Cell(7, 1, new Knight(true));
+        cells[7][2] = new Cell(7, 2, new Bishop(true));
+        cells[7][3] = new Cell(7, 3, new King(true));
+        cells[7][4] = new Cell(7, 4, new Queen(true));
+        cells[7][5] = new Cell(7, 5, new Bishop(true));
+        cells[7][6] = new Cell(7, 6, new Knight(true));
+        cells[7][7] = new Cell(7, 7, new Rook(true));
+
+        //  Kalan hucrelerin tas olmadan baslatilmasi
+        for (int x = 2; x < 6; x++){
+            for (int y = 0; y < 8; y++){
+                cells[x][y] = new Cell(x, y, null);
+            }
+        }
     }
 }
