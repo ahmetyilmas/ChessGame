75e1e93f9080e23552b232d9bd78e5454f5f07bd
ahmetyilmas <ahmetyilmas03@gmail.com>
2023-12-09 23:07:23 +0300

    initial commit

---
+++ ChessGame/src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    public boolean whiteSide;
+    Player(boolean whiteSide){
+        this.whiteSide = whiteSide;
+    }
+    public boolean isWhiteSide(){
+        return this.whiteSide;
+    }
+}
