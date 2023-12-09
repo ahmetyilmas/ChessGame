5359f127d94561e38e9e6e36394c56af4688fa3f
ahmetyilmas <ahmetyilmas03@gmail.com>
2023-12-09 20:01:51 +0300

    Initial commit

---
+++ ChessGame/src/Queen.java
@@ -0,0 +1,65 @@
+public class Queen extends Piece {
+
+    public Queen(boolean white) {
+        super(white);
+    }
+
+    @Override
+    public boolean canMove(Cell start, Cell destination, Board board) {
+
+        // Hedef hücrede aynı renkte başka bir taş varsa
+        if (start.getPiece().isWhite() == destination.getPiece().isWhite()) {
+            return false;
+        }
+
+        int diffX = Math.abs(destination.getX() - start.getX());
+        int diffY = Math.abs(destination.getY() - start.getY());
+
+        // yatay, dikey veya çapraz hareket edebilir
+        if ((diffX == 0 && diffY > 0) || (diffX > 0 && diffY == 0) || (diffX == diffY)) {
+            // dikey hareket
+            if (diffX == 0) {
+                int yDir = (destination.getY() - start.getY()) > 0 ? 1 : -1;
+                int y = start.getY() + yDir;
+
+                // yolda taş var mı
+                while (y != destination.getY()) {
+                    if (board.getCell(start.getX(), y).getPiece() != null) {
+                        return false;
+                    }
+                    y += yDir;
+                }
+            }
+            // yatay hareket
+            else if (diffY == 0) {
+                int xDir = (destination.getX() - start.getX()) > 0 ? 1 : -1;
+                int x = start.getX() + xDir;
+
+                // yolda taş var mı
+                while (x != destination.getX()) {
+                    if (board.getCell(x, start.getY()).getPiece() != null) {
+                        return false;
+                    }
+                    x += xDir;
+                }
+            }
+            // çapraz hareket
+            else {
+                int xDir = (destination.getX() - start.getX()) > 0 ? 1 : -1;
+                int yDir = (destination.getY() - start.getY()) > 0 ? 1 : -1;
+                int x = start.getX() + xDir;
+                int y = start.getY() + yDir;
+
+                //yolda taşi var mı
+                while (x != destination.getX() && y != destination.getY()) {
+                    if (board.getCell(x, y).getPiece() != null) {
+                        return false;
+                    }
+                    x += xDir;
+                    y += yDir;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ ChessGame/src/Rook.java
@@ -0,0 +1,56 @@
+public class Rook extends Piece {
+
+    private boolean castlingDone;   //Rok yapip yapmama durununu kontrol etmek icin degisken.
+    public Rook(boolean white) {
+        super(white);
+    }
+
+    public void setCastlingDone(boolean castlingDone){
+        this.castlingDone = castlingDone;
+    }
+    public boolean hasCastlingDone(){
+        return this.castlingDone;
+    }
+    @Override
+    public boolean canMove(Cell start, Cell destination, Board board) {
+
+        // Hedef hücrede aynı renkte başka bir taş varsa
+        if (start.getPiece().isWhite() == destination.getPiece().isWhite()) {
+            return false;
+        }
+
+        int diffX = Math.abs(destination.getX() - start.getX());
+        int diffY = Math.abs(destination.getY() - start.getY());
+
+        // sadece x ve ya sadece y koordinatı değişeceği için biri sabit
+        if ((diffX == 0 && diffY > 0) || (diffX > 0 && diffY == 0)) {
+            //dikey hareketinde
+            if (diffX == 0) {
+                int yDir = (destination.getY() - start.getY()) > 0 ? 1 : -1;
+                int y = start.getY() + yDir;
+
+                // yol boyunca taş var mı
+                while (y != destination.getY()) {
+                    if (board.getCell(start.getX(), y).getPiece() != null) {
+                        return false; // yolda taş varsa gidemez
+                    }
+                    y += yDir;
+                }
+            }
+            // yatay hareket
+            if (diffY == 0){
+                int xDir = (destination.getX() - start.getX()) > 0 ? 1 : -1;
+                int x = start.getX() + xDir;
+
+                // yol boyunca taş var mı
+                while (x != destination.getX()) {
+                    if (board.getCell(x, start.getY()).getPiece() != null) {
+                        return false; // yolda taş varsa gidemez
+                    }
+                    x += xDir;
+                }
+            }
+        }
+        return false;
+    }
+}
